6322e38da25eaaef15f5f86c200c5dd89c97f7a7
dabishman <jaybishtawi@outlook.com>
2022-04-24 19:52:10 -0600

    Adjust config

--- apps/ng-client/jest.config.ts
+++ apps/ng-client/jest.config.ts
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'ng-client',
+  displayName: 'Angular Client Jest Tests',
   preset: '../../jest.preset.ts',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -17,5 +17,14 @@ module.exports = {
     'jest-preset-angular/build/serializers/no-ng-attributes',
     'jest-preset-angular/build/serializers/ng-snapshot',
     'jest-preset-angular/build/serializers/html-comment'
-  ]
+  ],
+  coverageReporters: ['lcov', 'text', 'text-summary'],
+  coverageThreshold: {
+    global: {
+      branches: 85,
+      functions: 85,
+      lines: 85,
+      statements: 85
+    }
+  }
 };
