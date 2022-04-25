43f47841e9e9d0d50dc6fb5f1053972b27381f6e
dabishman <jaybishtawi@outlook.com>
2022-04-25 12:01:35 -0600

    More cleanup

--- apps/Clients/ng-client/src/app/app.component.ts
+++ apps/Clients/ng-client/src/app/app.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'ngx-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  title = 'ngx-app';
-
-  ngOnInit() {
-    // eslint-disable-next-line no-restricted-syntax
-    console.info(`AppComponent.ngOnInit()`);
-  }
+export class AppComponent {
+  title = 'ng-client';
 }
